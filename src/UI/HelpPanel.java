eec6064a4739ac86e523cfc8920c776a52a9783f
Katrina Scott <scott.268@wright.edu>
2015-11-19 16:27:44 -0500

    Temporary commit- working on help panel

--- src/UI/HelpPanel.java
+++ src/UI/HelpPanel.java
@@ -3,6 +3,7 @@ package src.UI;
 import java.awt.Color;
 
 import javax.swing.JPanel;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
@@ -19,16 +20,58 @@ public class HelpPanel extends JPanel {
 	public HelpPanel() {
 		
 		setBackground(Color.DARK_GRAY);
-		setLayout(new BorderLayout(0, 0));
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
-		// that's it- that's programming in a nutshell
-		JLabel lblHelp = new JLabel(" HELP");
-		lblHelp.setHorizontalAlignment(SwingConstants.LEFT);
-		lblHelp.setVerticalAlignment(SwingConstants.TOP);
-		lblHelp.setFont(new Font("DejaVu Sans", Font.BOLD, 30));
-		lblHelp.setForeground(Color.WHITE);
-		add(lblHelp);
+		JLabel header = createHeader();
+		
+		JLabel section1 = createSection1();
+		JLabel section2 = createSection2();
+		// etc etc fill these in
+		
+		add(header);
+		add(section1);
+		add(section2);
 		
 	} // end constructor
+	
+	private JLabel createHeader() {
+		
+		JLabel help = new JLabel(" HELP");
+		help.setHorizontalAlignment(SwingConstants.LEFT);
+		help.setVerticalAlignment(SwingConstants.TOP);
+		help.setFont(new Font("DejaVu Sans", Font.BOLD, 30));
+		help.setForeground(Color.WHITE);
+		
+		return help;
+		
+	} // end of method createHeader
+	
+	private JLabel createSection1() {
+		
+		JLabel section1 = new JLabel("Section 1");
+		//section1.setHorizontalAlignment(SwingConstants.LEFT);
+		//section1.setVerticalAlignment(SwingConstants.TOP);
+		section1.setFont(new Font("DejaVu Sans", Font.PLAIN, 15));
+		section1.setForeground(Color.WHITE);
+		
+		// open jtextareas upon clicking
+		
+		return section1;
+		
+	} // end of method createSection1
+	
+	private JLabel createSection2() {
+		
+		JLabel section2 = new JLabel("Section 2");
+		//section1.setHorizontalAlignment(SwingConstants.LEFT);
+		//section1.setVerticalAlignment(SwingConstants.TOP);
+		section2.setFont(new Font("DejaVu Sans", Font.PLAIN, 15));
+		section2.setForeground(Color.WHITE);
+		
+		// open jtextareas upon clicking
+		
+		return section2;
+		
+	} // end of method createSection2
 
 } // end class HelpPanel
