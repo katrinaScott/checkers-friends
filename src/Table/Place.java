0ab69ef9b263cdf5dbe34e382b6eca11bd718832
XanH <xanlan.h11@gmail.com>
2015-12-02 16:26:24 -0500

    Forgot to add Place.

---
+++ src/Table/Place.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.Table;
+
+import src.Chat.PublicChat;
+/**
+ *
+ * @author XanderH11
+ */
+public class CheckerPlace {
+    
+    public int exit(){
+        return 0;
+    }
+}
